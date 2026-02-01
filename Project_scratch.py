93ba93a6e4732344b39cfbdbebd30ecca13477bd
umaraneeqa-glitch <umaraneeqa@googlemail.com>
2026-02-01 09:36:47 +0000

    Create initial inventory dictionaries for drinks, books, and food
    Initialized inventories for drinks, books, and food with details.

---
+++ Project_scratch.py
@@ -0,0 +1,113 @@
+
+# Define Drinks inventory in a dictionary
+
+drink_inventory = {
+
+    'drinkone' : { 
+        'drink': 'flat white', 
+        'price': '3.60' , 
+        'stock' : '10' , 
+        'details' : 'milk + cofee'},
+    
+    'drinktwo' : {
+        'drink': 'cappacino', 
+        'price': '3.00' , 
+        'stock' : '10' , 
+        'details' : 'milk + cofee'},
+
+     'drinkthree' : {
+        'drink': 'herbal tea', 
+        'price': '2.00' , 
+        'stock' : '10' , 
+        'details' : 'water + herbs'},
+    
+     'drinkfour' : {
+        'drink': 'English Tea', 
+        'price': '3.00' , 
+        'stock' : '10' , 
+        'details' : 'milk + tea'},
+    
+     'drinkfive' : {
+        'drink': 'Espresso', 
+        'price': '4.00' , 
+        'stock' : '10' , 
+        'details' : 'water + cofee'},
+
+}
+
+# Define books inventory in a dictionary
+
+book_inventory = {
+
+    'bookone' : { 
+        'book': 'Harry Potter', 
+        'price': '9.00' , 
+        'stock' : '5' , 
+        'author_type' : 'JK Rowling (Fiction)'
+    },
+
+    'booktwo' : { 
+        'book': '7 Habits of highly effective people', 
+        'price': '10.00' , 
+        'stock' : '4' , 
+        'author_type' : 'Stephen Cohen (Productivity)'
+    },
+
+    'bookthree' : { 
+        'book': 'Atomic Habits', 
+        'price': '7.00' , 
+        'stock' : '8' , 
+        'author_type' : 'James Clear (Productivity)'
+    },
+
+    'bookfour' : { 
+        'book': 'The diary of a CEO', 
+        'price': '7.00' , 
+        'stock' : '8' , 
+        'author_type' : 'Steven Bartlet (Autobiography)'
+    },
+
+    'bookfive' : { 
+        'book': 'Pride and Prejudice', 
+        'price': '5.00' , 
+        'stock' : '9' , 
+        'author_type' : 'Jane Austen (Fiction)'
+    }
+
+} 
+
+# Define Food Inventory in a dictionary
+
+food_inventory = {
+
+    'foodone' : { 
+        'food': 'Croissant', 
+        'price': '3.60' , 
+        'stock' : '10' , 
+        'details' : 'flour + water + Butter'},
+
+    'foodtwo' : { 
+        'food': 'Pesto Panini', 
+        'price': '5.00' , 
+        'stock' : '15' , 
+        'details' : 'bread + cheese + tomato + olive oil + basil + bine nuts + salt, warning : " may contain nuts!'},
+
+    'foodthree' : { 
+        'food': 'cheesecake', 
+        'price': '4.00' , 
+        'stock' : '12' , 
+        'details' : 'cheese + cream + sugar + butter + flour'},
+
+    'foodfour' : { 
+        'food': 'Cookie', 
+        'price': '3.60' , 
+        'stock' : '10' , 
+        'details' : 'flour + butter + chocolate + sugar'},
+    
+    'foodfive' : { 
+        'food': 'chips', 
+        'price': '3.60' , 
+        'stock' : '10' , 
+        'details' : 'potatoes + oil'},
+
+}
